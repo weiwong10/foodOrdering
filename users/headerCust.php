d157a0236fa991a2920eb51f800f9e29f21958af
lqie02 <tanleqie@gmail.com>
2023-06-24 03:16:45 +0800

    .

--- users/headerCust.php
+++ users/headerCust.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" type="png" href="#"> <!--icon-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="../css/try.css">

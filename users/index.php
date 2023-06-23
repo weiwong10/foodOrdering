d05f1b938cc864c574ffdf2d4dfa283b8a4c0b89
lqie02 <tanleqie@gmail.com>
2023-06-24 03:14:46 +0800

    .

--- users/index.php
+++ users/index.php
@@ -15,7 +15,6 @@ else{
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>Sweet Sensations</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
@@ -101,6 +100,7 @@ else{
         </div>
 
       </div>
+    </section>
     </section>
 
     <section id="contact" class="contact section-bg">
